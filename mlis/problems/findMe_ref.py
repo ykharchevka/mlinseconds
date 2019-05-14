7dd8e2af71dcc7733f5f575655a8ee2b5f2f08ea
Yehor <kharchevka@gmail.com>
2019-05-14 22:58:59 +0300

    initial version of findMe

--- mlis/problems/findMe_ref.py
+++ mlis/problems/findMe_ref.py
@@ -1,19 +1,3 @@
-'''
-No 10 steps condition for the task
-TODOs:
-1. Binnary_cross_entropy vs BCEloss
-2. BatchNorm1d + track running stats = False
-3. relu vs relu6
-4. relu vs prelu + -> [-1, 1] ->> [-0.5, 0.5] or [-4.5, 4.5]
-5. use model averaging to improve the accuracy
- - so instead of one [24, 24, 24] network I created an architecture
- of three different [24, 24, 24] chains of hidden layers
- combined with a [72, 1] layer on top
-6. use bilinear layers to get feature crosses (not helped with generalCpu)
- - mistake was doing this as one of the last steps, not one of the first steps
- - could've saved a lot of grid search time
-7. https://www.youtube.com/watch?v=BOCLq2gpcGU&list=PLkDaE6sCZn6Hn0vK8co82zjQtt3T2Nkqc&index=8
-'''
 # There are random function from 8 inputs and X random inputs added.
 # We split data in 2 parts, on first part you will train and on second
 # part we will test
@@ -25,41 +9,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from ..utils import solutionmanager as sm
-from ..utils.gridsearch import GridSearch
 
 class SolutionModel(nn.Module):
-    def __init__(self, input_size, output_size, solution):
+    def __init__(self, input_size, output_size):
         super(SolutionModel, self).__init__()
         self.input_size = input_size
-        self._s = solution
-        hidden_layers = []
-        for i in range(self._s.hidden_depth):
-            hidden_layers.extend([
-                nn.Linear(self._s.hidden_size, self._s.hidden_size),
-                nn.Tanh()
-            ])
-        self.model = nn.Sequential(
-            nn.Linear(self.input_size, self._s.hidden_size),
-            nn.Tanh(),
-            *hidden_layers,
-            nn.Linear(self._s.hidden_size, output_size),
-            nn.Sigmoid(),
-        )
-        for param in self.model.parameters():
-            nn.init.uniform_(param, -self._s.init, +self._s.init)
+        self.hidden_size = 32
+        self.linear1 = nn.Linear(input_size, self.hidden_size)
+        self.linear2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.linear3 = nn.Linear(self.hidden_size, output_size)
 
     def forward(self, x):
-        return self.model.forward(x)
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        x = F.relu(x)
+        x = self.linear3(x)
+        x = torch.sigmoid(x)
+        return x
 
     def calc_loss(self, output, target):
-        loss_fn = nn.BCELoss()
-        loss = None
-        try:
-            loss = loss_fn(output, target)
-        except RuntimeError:
-            print('Runtime error at hidden depth: {}, hidden size: {}, lr: {}, alpha: {}, momentum: {}'.
-                  format(self._s.hidden_depth, self._s.hidden_size, self._s.lr, self._s.alpha, self._s.momentum))
-            exit()
+        bce_loss = nn.BCELoss()
+        loss = bce_loss(output, target)
         return loss
 
     def calc_predict(self, output):
@@ -68,35 +39,22 @@ class SolutionModel(nn.Module):
 
 class Solution():
     def __init__(self):
-        self.hidden_depth = 2  # 3
-        self.hidden_depth_grid = [3]
-        self.hidden_size = 50  # 50
-        self.hidden_size_grid = [50]
-        self.init = 1.
-        self.init_grid = [1.]
-        self.lr = 0.001
-        self.lr_grid = [0.001]
-        self.alpha = 0.99
-        self.alpha_grid = [0.99]
-        self.momentum = 0.7
-        self.momentum_grid = [0.7]
-        self.grid_search = GridSearch(self).set_enabled(False)
+        self = self
 
     def create_model(self, input_size, output_size):
-        return SolutionModel(input_size, output_size, self)
+        return SolutionModel(input_size, output_size)
 
     # Return number of steps used
     def train_model(self, model, train_data, train_target, context):
         step = 0
         # Put model in train mode
         model.train()
-        optimizer = optim.RMSprop(model.parameters(), lr=self.lr, alpha=self.alpha, eps=1e-08, weight_decay=0,
-                                  momentum=self.momentum, centered=False)
         while True:
             time_left = context.get_timer().get_time_left()
             # No more time left, stop training
             if time_left < 0.1:
                 break
+            optimizer = optim.SGD(model.parameters(), lr=1)
             data = train_data
             target = train_target
             # model.parameters()...gradient set to zero
@@ -111,7 +69,6 @@ class Solution():
             total = predict.view(-1).size(0)
             # calculate loss
             loss = model.calc_loss(output, target)
-            self.grid_search.log_step_value('loss', loss.item(), step)
             # calculate deriviative of model.forward() and put it in model.parameters()...gradient
             loss.backward()
             # print progress of the learning
@@ -133,7 +90,7 @@ class Solution():
 class Limits:
     def __init__(self):
         self.time_limit = 2.0
-        self.size_limit = 1_000_000
+        self.size_limit = 1000000
         self.test_limit = 1.0
 
 class DataProvider:
@@ -142,7 +99,7 @@ class DataProvider:
 
     def create_data(self, data_size, input_size, random_input_size, seed):
         torch.manual_seed(seed)
-        function_size = 1 << input_size  # 256
+        function_size = 1 << input_size
         function_input = torch.ByteTensor(function_size, input_size)
         for i in range(function_input.size(0)):
             fun_ind = i
@@ -175,18 +132,16 @@ class DataProvider:
         return (data.float(), target.view(-1, 1).float())
 
     def create_case_data(self, case):
-        data_size = 256*32  # 8_192
+        data_size = 256*32
         input_size = 8
-        random_input_size = min(32, (case-1)*4)  # 0...32 as case is up to 10
+        random_input_size = min(32, (case-1)*4)
 
         data, target = self.create_data(2*data_size, input_size, random_input_size, case)
-        return sm.CaseData(
-            case, Limits(), (data[:data_size], target[:data_size]), (data[data_size:], target[data_size:])
-        ).set_description("{} inputs and {} random inputs".format(input_size, random_input_size))
+        return sm.CaseData(case, Limits(), (data[:data_size], target[:data_size]), (data[data_size:], target[data_size:])).set_description("{} inputs and {} random inputs".format(input_size, random_input_size))
 
 class Config:
     def __init__(self):
-        self.max_samples = 10_000
+        self.max_samples = 10000
 
     def get_data_provider(self):
         return DataProvider()
@@ -195,4 +150,4 @@ class Config:
         return Solution()
 
 # If you want to run specific case, put number here
-sm.SolutionManager(Config()).run(case_number=10)
+sm.SolutionManager(Config()).run(case_number=-1)
