f4247187ab7bf7d0b0f80468267a0df303232d45
Yehor Kharchevka <ykh@artificialcore.com>
2019-05-20 10:13:02 +0300

    file to show diff

---
+++ mlis/problems/findMe_done.py
@@ -0,0 +1,161 @@
+# There are random function from 8 inputs and X random inputs added.
+# We split data in 2 parts, on first part you will train and on second
+# part we will test
+import time
+import random
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from ..utils import solutionmanager as sm
+
+
+class SolutionModel(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(SolutionModel, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = 32
+        self.linear1 = nn.Linear(input_size, self.hidden_size)
+        self.linear2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.linear3 = nn.Linear(self.hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        x = F.relu(x)
+        x = self.linear3(x)
+        x = torch.sigmoid(x)
+        return x
+
+    def calc_loss(self, output, target):
+        bce_loss = nn.BCELoss()
+        loss = bce_loss(output, target)
+        return loss
+
+    def calc_predict(self, output):
+        predict = output.round()
+        return predict
+
+
+class Solution():
+    def __init__(self):
+        self = self
+
+    def create_model(self, input_size, output_size):
+        return SolutionModel(input_size, output_size)
+
+    # Return number of steps used
+    def train_model(self, model, train_data, train_target, context):
+        step = 0
+        # Put model in train mode
+        model.train()
+        while True:
+            time_left = context.get_timer().get_time_left()
+            # No more time left, stop training
+            if time_left < 0.1:
+                break
+            optimizer = optim.SGD(model.parameters(), lr=1)
+            data = train_data
+            target = train_target
+            # model.parameters()...gradient set to zero
+            optimizer.zero_grad()
+            # evaluate model => model.forward(data)
+            output = model(data)
+            # if x < 0.5 predict 0 else predict 1
+            predict = model.calc_predict(output)
+            # Number of correct predictions
+            correct = predict.eq(target.view_as(predict)).long().sum().item()
+            # Total number of needed predictions
+            total = predict.view(-1).size(0)
+            # calculate loss
+            loss = model.calc_loss(output, target)
+            # calculate deriviative of model.forward() and put it in model.parameters()...gradient
+            loss.backward()
+            # print progress of the learning
+            self.print_stats(step, loss, correct, total)
+            # update model: model.parameters() -= lr * gradient
+            optimizer.step()
+            step += 1
+        return step
+
+    def print_stats(self, step, loss, correct, total):
+        if step % 100 == 0:
+            print("Step = {} Prediction = {}/{} Error = {}".format(step, correct, total, loss.item()))
+
+
+###
+###
+### Don't change code after this line
+###
+###
+class Limits:
+    def __init__(self):
+        self.time_limit = 2.0
+        self.size_limit = 1000000
+        self.test_limit = 1.0
+
+
+class DataProvider:
+    def __init__(self):
+        self.number_of_cases = 10
+
+    def create_data(self, data_size, input_size, random_input_size, seed):
+        torch.manual_seed(seed)
+        function_size = 1 << input_size
+        function_input = torch.ByteTensor(function_size, input_size)
+        for i in range(function_input.size(0)):
+            fun_ind = i
+            for j in range(function_input.size(1)):
+                input_bit = fun_ind & 1
+                fun_ind = fun_ind >> 1
+                function_input[i][j] = input_bit
+        function_output = torch.ByteTensor(function_size).random_(0, 2)
+
+        if data_size % function_size != 0:
+            raise "Data gen error"
+
+        data_input = torch.ByteTensor(data_size, input_size).view(-1, function_size, input_size)
+        target = torch.ByteTensor(data_size).view(-1, function_size)
+        for i in range(data_input.size(0)):
+            data_input[i] = function_input
+            target[i] = function_output
+        data_input = data_input.view(data_size, input_size)
+        target = target.view(data_size)
+        if random_input_size > 0:
+            data_random = torch.ByteTensor(data_size, random_input_size).random_(0, 2)
+            data = torch.cat([data_input, data_random], dim=1)
+        else:
+            data = data_input
+        perm = torch.randperm(data.size(1))
+        data = data[:, perm]
+        perm = torch.randperm(data.size(0))
+        data = data[perm]
+        target = target[perm]
+        return (data.float(), target.view(-1, 1).float())
+
+    def create_case_data(self, case):
+        data_size = 256 * 32
+        input_size = 8
+        random_input_size = min(32, (case - 1) * 4)
+
+        data, target = self.create_data(2 * data_size, input_size, random_input_size, case)
+        return sm.CaseData(case, Limits(), (data[:data_size], target[:data_size]),
+                           (data[data_size:], target[data_size:])).set_description(
+            "{} inputs and {} random inputs".format(input_size, random_input_size))
+
+
+class Config:
+    def __init__(self):
+        self.max_samples = 10000
+
+    def get_data_provider(self):
+        return DataProvider()
+
+    def get_solution(self):
+        return Solution()
+
+
+# If you want to run specific case, put number here
+sm.SolutionManager(Config()).run(case_number=-1)
