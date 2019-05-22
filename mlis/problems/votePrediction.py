fcf07fecdebf8053215f62c872724f6efccf23a8
Yehor <kharchevka@gmail.com>
2019-05-22 07:04:22 +0300

    fixes in votePrediction

--- mlis/problems/votePrediction.py
+++ mlis/problems/votePrediction.py
@@ -17,53 +17,6 @@ TODOs:
    https://github.com/svoit/mlinseconds/commit/edc2dffa62ed74320c9d7c970e38c2764553b602?fbclid=IwAR1nu0frwNFeY9xCkkMeq6uxpHEF_yOzm3PI6peR1X-h15kAjxFT0T32i9U
 4. Maybe add "x = (x - 0.5)*9" to forward def beginning. Hint: we are not obliged to normalize targets same way as we normalize inputs!
 5. https://www.youtube.com/watch?v=QrzApibhohY&list=PLkDaE6sCZn6Hn0vK8co82zjQtt3T2Nkqc&index=13
-
-hL02_hs080_bs512_lr0.0010_mm0.90: Prediction = 1.0000, Loss = 0.0811, Time left = 0.2096, Step = 443
-hL02_hs064_bs512_lr0.0010_mm0.90: Prediction = 0.9766, Loss = 0.1051, Time left = 0.0974, Step = 578
-hL02_hs064_bs1024_lr0.0010_mm0.90: Prediction = 1.0000, Loss = 0.1084, Time left = 0.6469, Step = 282
-hL02_hs160_bs512_lr0.0010_mm0.90: Prediction = 1.0000, Loss = 0.1124, Time left = 0.1853, Step = 311
-hL02_hs080_bs1024_lr0.0010_mm0.90: Prediction = 1.0000, Loss = 0.1222, Time left = 0.8184, Step = 214
-hL02_hs160_bs256_lr0.0010_mm0.90: Prediction = 0.9648, Loss = 0.1314, Time left = 0.0951, Step = 468
-hL02_hs064_bs512_lr0.0005_mm0.90: Prediction = 0.9824, Loss = 0.1326, Time left = 0.0972, Step = 574
-hL02_hs160_bs1024_lr0.0010_mm0.90: Prediction = 1.0000, Loss = 0.1580, Time left = 0.3062, Step = 161
-hL02_hs080_bs512_lr0.0005_mm0.90: Prediction = 0.9902, Loss = 0.1591, Time left = 0.0973, Step = 524
-hL02_hs064_bs256_lr0.0010_mm0.90: Prediction = 0.9414, Loss = 0.1625, Time left = 0.0965, Step = 674
-hL02_hs080_bs256_lr0.0010_mm0.90: Prediction = 0.9414, Loss = 0.1632, Time left = 0.0968, Step = 607
-hL02_hs064_bs1024_lr0.0005_mm0.90: Prediction = 1.0000, Loss = 0.1723, Time left = 0.3141, Step = 333
-hL02_hs160_bs256_lr0.0005_mm0.90: Prediction = 0.9805, Loss = 0.1847, Time left = 0.0957, Step = 455
-hL02_hs080_bs256_lr0.0005_mm0.90: Prediction = 0.9688, Loss = 0.1906, Time left = 0.0963, Step = 569
-hL02_hs064_bs256_lr0.0005_mm0.90: Prediction = 0.9258, Loss = 0.2007, Time left = 0.0972, Step = 640
-hL02_hs160_bs512_lr0.0005_mm0.90: Prediction = 0.9922, Loss = 0.2103, Time left = 0.0937, Step = 322
-hL02_hs080_bs1024_lr0.0005_mm0.90: Prediction = 1.0000, Loss = 0.2206, Time left = 0.4514, Step = 257
-hL02_hs320_bs512_lr0.0010_mm0.90: Prediction = 0.9316, Loss = 0.2318, Time left = 0.0888, Step = 185
-hL02_hs160_bs1024_lr0.0005_mm0.90: Prediction = 1.0000, Loss = 0.2550, Time left = 0.2254, Step = 165
-hL02_hs320_bs256_lr0.0010_mm0.90: Prediction = 0.8867, Loss = 0.3002, Time left = 0.0924, Step = 292
-hL02_hs320_bs512_lr0.0005_mm0.90: Prediction = 0.9219, Loss = 0.3085, Time left = 0.0909, Step = 183
-hL02_hs320_bs1024_lr0.0010_mm0.90: Prediction = 0.8740, Loss = 0.3509, Time left = 0.0768, Step = 88
-hL02_hs320_bs256_lr0.0005_mm0.90: Prediction = 0.8711, Loss = 0.3651, Time left = 0.0930, Step = 288
-hL02_hs320_bs1024_lr0.0005_mm0.90: Prediction = 0.9141, Loss = 0.3665, Time left = 0.0777, Step = 106
-hL02_hs080_bs512_lr0.0001_mm0.90: Prediction = 0.9785, Loss = 0.3890, Time left = 0.0964, Step = 508
-hL02_hs064_bs512_lr0.0001_mm0.90: Prediction = 0.9395, Loss = 0.3945, Time left = 0.0965, Step = 584
-hL02_hs064_bs256_lr0.0001_mm0.90: Prediction = 0.8711, Loss = 0.4119, Time left = 0.0957, Step = 683
-hL02_hs080_bs256_lr0.0001_mm0.90: Prediction = 0.9102, Loss = 0.4233, Time left = 0.0944, Step = 568
-hL02_hs064_bs1024_lr0.0001_mm0.90: Prediction = 0.9482, Loss = 0.4325, Time left = 0.0951, Step = 398
-hL02_hs080_bs1024_lr0.0001_mm0.90: Prediction = 0.9766, Loss = 0.4492, Time left = 0.0950, Step = 333
-hL02_hs160_bs512_lr0.0001_mm0.90: Prediction = 1.0000, Loss = 0.4501, Time left = 0.2610, Step = 291
-hL02_hs160_bs256_lr0.0001_mm0.90: Prediction = 1.0000, Loss = 0.4550, Time left = 0.3556, Step = 377
-hL02_hs320_bs256_lr0.0001_mm0.90: Prediction = 0.9297, Loss = 0.5113, Time left = 0.0948, Step = 278
-hL02_hs160_bs1024_lr0.0001_mm0.90: Prediction = 1.0000, Loss = 0.5139, Time left = 0.5265, Step = 145
-hL02_hs320_bs512_lr0.0001_mm0.90: Prediction = 0.9531, Loss = 0.5399, Time left = 0.0889, Step = 195
-hL02_hs640_bs1024_lr0.0005_mm0.90: Prediction = 0.7559, Loss = 0.5699, Time left = 0.0457, Step = 38
-hL02_hs640_bs512_lr0.0005_mm0.90: Prediction = 0.7500, Loss = 0.5712, Time left = 0.0800, Step = 70
-hL02_hs640_bs1024_lr0.0010_mm0.90: Prediction = 0.6973, Loss = 0.5781, Time left = 0.0500, Step = 41
-hL02_hs640_bs256_lr0.0005_mm0.90: Prediction = 0.6836, Loss = 0.5830, Time left = 0.0870, Step = 132
-hL02_hs320_bs1024_lr0.0001_mm0.90: Prediction = 0.9287, Loss = 0.5908, Time left = 0.0860, Step = 88
-hL02_hs640_bs256_lr0.0001_mm0.90: Prediction = 0.7656, Loss = 0.5949, Time left = 0.0873, Step = 140
-hL02_hs640_bs512_lr0.0010_mm0.90: Prediction = 0.7031, Loss = 0.5978, Time left = 0.0751, Step = 88
-hL02_hs640_bs512_lr0.0001_mm0.90: Prediction = 0.7949, Loss = 0.6109, Time left = 0.0678, Step = 77
-hL02_hs640_bs256_lr0.0010_mm0.90: Prediction = 0.6680, Loss = 0.6246, Time left = 0.0845, Step = 143
-hL02_hs640_bs1024_lr0.0001_mm0.90: Prediction = 0.8271, Loss = 0.6326, Time left = 0.0468, Step = 39
->>>>>>> WiP on votePrediction
 """
 import time
 import random
@@ -155,15 +108,17 @@ class Solution():
             'leakyrelu01': nn.LeakyReLU(0.1)
         }
         self.layers_number = 2
-        self.layers_number_grid = [2]
+        self.layers_number_grid = [2, 3]
         self.hidden_size = 80
-        self.hidden_size_grid = [64, 80, 160, 320, 640]
-        self.batch_size = 256
-        self.batch_size_grid = [256, 512, 1024]
+        self.hidden_size_grid = [60, 70, 80, 100]
+        self.batch_size = 512
+        self.batch_size_grid = [16, 128, 512]
+        self.batch_loss = 1e-7
+        self.batch_loss_grid = [1e-9, 1e-5, 0.1, 0.3]
         self.learning_rate = 0.001
-        self.learning_rate_grid = [0.0001, 0.0005, 0.001]
+        self.learning_rate_grid = [0.0005, 0.001, 0.01]
         self.momentum = 0.9  # 0.8
-        self.momentum_grid = [0.9]
+        self.momentum_grid = [0.8, 0.9]
         self.weight_decay = 0.
         self.weight_decay_grid = [0.0]
         self.do_batch_norm = True
@@ -187,7 +142,7 @@ class Solution():
         return SolutionModel(input_size, output_size, self)
 
     def get_key(self):
-        return "hL{0:02d}_hs{1:03d}_bs{2:03d}_lr{3:.4f}_mm{4:.2f}".format(
+        return "hL{0:02d}_hs{1:04d}_bs{2:03d}_lr{3:.4f}_mm{4:.2f}".format(
             self.layers_number, self.hidden_size, self.batch_size, self.learning_rate, self.momentum)
 
     # Return number of steps used
@@ -228,7 +183,7 @@ class Solution():
             total = predict.view(-1).size(0)
 
             diff = (output.data - target.data).abs()
-            if diff.max() < 0.3:
+            if diff.max() < self.batch_loss:
                 goodCount += 1
                 if goodCount >= goodLimit:
                     goodBreak = True
@@ -236,11 +191,16 @@ class Solution():
                 goodCount = 0
 
             if (
-                    correct == total or
+                    # correct == total or
                     time_left < 0.1 or
                     goodBreak
                     # or (self.grid_search.enabled and step > 2000)
             ):
+                output = model(train_data)
+                predict = model.calc_predict(output)
+                correct = predict.eq(train_target.view_as(predict)).long().sum().item()
+                total = predict.view(-1).size(0)
+                loss = model.calc_loss(output, train_target)
                 if not key in self.sols:
                     self.sols[key] = 0
                     self.worst_prediction[key] = correct / total
